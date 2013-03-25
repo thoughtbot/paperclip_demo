7aec80e94e3a02c270e86e22b12323cf620b605f
Mary Schmidt <marykateschmidt@gmail.com>
2013-03-25 10:42:45 -0400

    refactor friends_controller and views

--- spec/acceptance/visitor_adds_new_friend_spec.rb
+++ spec/acceptance/visitor_adds_new_friend_spec.rb
@@ -5,7 +5,7 @@ feature 'visitor adds a new friend' do
     visit root_path
     click_link 'New Friend'
     fill_in 'Name', with: 'Mona Lisa'
-    attach_file 'Avatar', Rails.root.join('spec', 'support', 'mona_lisa.jpg')
+    attach_file 'Avatar', 'spec/support/mona_lisa.jpg'
     click_button 'Make a friend'
 
     page.should have_css('h2', text: 'Mona Lisa')
