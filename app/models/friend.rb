bb6fd70125e0e63cd467ef989c807c8dcb0da603
Dan Collis-Puro <djcp@thoughtbot.com>
2013-09-13 12:56:35 -0400

    Add Paperclip::Shoulda::Matchers demonstrations

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,11 +1,12 @@
 class Friend < ActiveRecord::Base
   validates :name, presence: true
+  validates :avatar,
+    attachment_content_type: { content_type: /\Aimage\/.*\Z/ },
+    attachment_size: { less_than: 5.megabytes }
 
   has_attached_file :avatar, styles: {
     thumb: '100x100>',
     square: '200x200#',
     medium: '300x300>'
   }
-
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Friend do
+  include Paperclip::Shoulda::Matchers
   it { should validate_presence_of :name }
+
+  it { should_not validate_attachment_presence :avatar }
+  it { should validate_attachment_content_type(:avatar).allowing(
+      'image/png', 'image/jpg', 'image/jpeg'
+    ).rejecting(
+      'text/plain', 'text/html'
+    )
+  }
+  it { should validate_attachment_size(:avatar).less_than(5.megabytes) }
 end
